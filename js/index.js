41dd357eb4aedfae0ad71b7c231f60fa86445fb6
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-13 14:10:46 -0600

    finished task 2, HTML updates from JSON finished

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
-const navItems = document.querySelectorAll('header nav');
+const navItems = document.querySelectorAll('header nav a');
 const header = document.querySelector('header');
 const headLine = document.querySelector('.cta-text h1');
 const button = document.querySelector('.cta-text button');
@@ -53,3 +53,54 @@ const bottomContentP = document.querySelectorAll('.bottom-content .text-content 
 const contactH4 = document.querySelector('.contact h4');
 const contactP = document.querySelectorAll('.contact p')
 const footerP = document.querySelector('footer p')
+
+// Building nav items
+navItems[0].textContent = 'Services'
+navItems[1].textContent = 'Product'
+navItems[2].textContent = 'Vision'
+navItems[3].textContent = 'Features'
+navItems[4].textContent = 'About'
+navItems[5].textContent = 'Contact'
+
+// Build CTA Content
+const headLine1 = document.createElement('p');
+const headLine2 = document.createElement('p');
+const headLine3 = document.createElement('p');
+headLine1.textContent = 'DOM';
+headLine2.textContent = 'IS';
+headLine3.textContent = 'AWESOME';
+headLine.appendChild(headLine1);
+headLine.appendChild(headLine2);
+headLine.appendChild(headLine3);
+button.textContent = 'Get Started';
+circleImg.src = 'img/header-img.png';
+
+// Build Top Content
+topContentH4[0].textContent = 'FEATURES';
+topContentH4[1].textContent = 'ABOUT';
+topContentP[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+topContentP[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+// Middle Image
+middleImg.src = 'img/mid-page-accent.jpg';
+
+// Build Bottom Content
+bottomContentH4[0].textContent = 'SERVICES';
+bottomContentH4[1].textContent = 'PRODUCT';
+bottomContentH4[2].textContent = 'VISION';
+bottomContentP[0].textContent = 'Service content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomContentP[1].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+bottomContentP[2].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+// Build Contact
+contactH4.textContent = 'CONTACT';
+contactP[0].textContent = '123 Way 456 Street';
+contactP[1].textContent = 'Somewhere, USA';
+contactP[2].textContent = '1 (888) 888-8888'
+const contactEmail = document.createElement('p');
+contactEmail.textContent = 'sales@greatidea.io';
+contactP[2].append(contactEmail)
+
+// Build Footer
+footerP.textContent = 'Copyright Great Idea! 2018'
+// console.log(contactP)
